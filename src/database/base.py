bc5fda3291a12e5c474baa0c8f432193390f3732
Lucas Hage <lucashage1995@gmail.com>
2024-09-20 12:48:17 -0300

    feat: add sqlalchemy base and session modules

---
+++ src/database/base.py
@@ -0,0 +1,11 @@
+import sqlalchemy as sql
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+
+
+@as_declarative()
+class Base(object):
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    id = sql.Column(sql.Uuid, primary_key=True)

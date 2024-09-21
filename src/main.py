53c04e3ae3567e648c1b7e439dc9cef35588fa23
Lucas Hage <lucashage1995@gmail.com>
2024-09-21 09:37:50 -0300

    feat? add context manager

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from src.database.base import Base
+from src.database.session import engine
 from .users.routers import users
 
+
 app = FastAPI()
+
+
+@asynccontextmanager
+async def app_lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+
+    yield
+
+
+app = FastAPI(lifespan=app_lifespan)
 app.include_router(users.router)
